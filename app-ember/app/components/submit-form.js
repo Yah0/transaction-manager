4df789f72ec0684a7f4a785bfcca57815e66f2f9
Jan Klinger <jan.klinger@typedflow.com>
2023-04-01 16:55:00 +0200

    create uuid generator for form

--- app-ember/app/components/submit-form.js
+++ app-ember/app/components/submit-form.js
@@ -1,6 +1,17 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { v4 as uuidv4 } from 'ember-uuid';
+import { tracked } from '@glimmer/tracking';
 
 export default class SubmitFormComponent extends Component {
+  @tracked accountId = '';
+
+  @action
+  generateUUID() {
+    const newUUID = uuidv4();
+    this.accountId = newUUID;
+  }
+
   submitTransaction(accountId, amount) {
     console.log(accountId, amount);
   }
