d742369e126cd1fe8033f3ccb3aeb1a901539a86
Jan Klinger <jan.klinger@typedflow.com>
2023-04-03 15:34:29 +0200

    create onSubmit method

--- app-ember/app/components/submit-form.js
+++ app-ember/app/components/submit-form.js
@@ -2,17 +2,56 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { v4 as uuidv4 } from 'ember-uuid';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
 
 export default class SubmitFormComponent extends Component {
-  @tracked accountId = '';
+  @service store;
 
-  @action
-  generateUUID() {
-    const newUUID = uuidv4();
-    this.accountId = newUUID;
+  @tracked accountId = '';
+  @tracked amount = 0;
+
+  isValidUUID(uuid) {
+    const uuidRegex =
+      /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
+    return uuidRegex.test(uuid);
   }
 
-  submitTransaction(accountId, amount) {
-    console.log(accountId, amount);
+  @action
+  generateRandomUUID() {
+    this.accountId = uuidv4();
+  }
+
+  @action
+  async onSubmit() {
+    const accountId = this.accountId;
+    const amount = this.amount;
+
+    let account = this.store.peekRecord('account', accountId);
+    if (account) {
+      account.balance += amount;
+    }
+
+    // Create a new submitted-transaction record
+    let submittedTransaction = this.store.createRecord('transaction', {
+      account_id: accountId,
+      amount: amount,
+    });
+
+    try {
+      // Save the new submitted transaction to the server
+      let response = await submittedTransaction.save();
+
+      // Update the submitted transaction with the response data
+      submittedTransaction.setProperties({
+        id: response.id,
+        created_at: response.created_at,
+      });
+
+      // Clear the form
+      this.accountId = '';
+      this.amount = '';
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
