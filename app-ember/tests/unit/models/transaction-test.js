42730e9929608ed830c5e8c68332f35de0dd0c18
Jan Klinger <jan.klinger@typedflow.com>
2023-04-10 17:53:49 +0200

    delete comments

--- app-ember/tests/unit/models/transaction-test.js
+++ app-ember/tests/unit/models/transaction-test.js
@@ -5,7 +5,6 @@ import { setupTest } from 'app-ember/tests/helpers';
 module('Unit | Model | transaction', function (hooks) {
   setupTest(hooks);
 
-  // Replace this with your real tests.
   test('it exists', function (assert) {
     let store = this.owner.lookup('service:store');
     let model = store.createRecord('transaction', {});
