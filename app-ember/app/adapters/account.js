c926635271a1da19d16337c0600905b3d93a2c14
Jan Klinger <jan.klinger@typedflow.com>
2023-04-10 12:00:47 +0200

    delete console.log

--- app-ember/app/adapters/account.js
+++ app-ember/app/adapters/account.js
@@ -4,7 +4,6 @@ export default class AccountAdapter extends ApplicationAdapter {
   namespace = '';
 
   urlForQueryRecord(query) {
-    console.log(query);
     const { accountId } = query;
     return `${this.namespace}/${accountId}`;
   }
