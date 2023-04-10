acf217b6bbd20e7f9d05e7947e39609e83052610
Jan Klinger <jan.klinger@typedflow.com>
2023-04-10 12:01:08 +0200

    create unit test for account adapter

--- app-ember/tests/unit/adapters/account-test.js
+++ app-ember/tests/unit/adapters/account-test.js
@@ -1,13 +1,15 @@
 import { module, test } from 'qunit';
-
-import { setupTest } from 'app-ember/tests/helpers';
+import { setupTest } from 'ember-qunit';
 
 module('Unit | Adapter | account', function (hooks) {
   setupTest(hooks);
 
-  // Replace this with your real tests.
-  test('it exists', function (assert) {
-    let adapter = this.owner.lookup('adapter:account');
-    assert.ok(adapter);
+  test('urlForQueryRecord returns the correct URL', function (assert) {
+    const adapter = this.owner.lookup('adapter:account');
+    const query = { accountId: '123' };
+    const expectedUrl = '/123';
+    const actualUrl = adapter.urlForQueryRecord(query);
+
+    assert.deepEqual(actualUrl, expectedUrl, 'The generated URL is correct');
   });
 });
