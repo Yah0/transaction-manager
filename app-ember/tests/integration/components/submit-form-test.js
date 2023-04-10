4b2d1bca0d2f08fc22eff6d280e8b2c08268b38c
Jan Klinger <jan.klinger@typedflow.com>
2023-04-10 16:26:35 +0200

    create integration test for submit-form

--- app-ember/tests/integration/components/submit-form-test.js
+++ app-ember/tests/integration/components/submit-form-test.js
@@ -1,26 +1,64 @@
 import { module, test } from 'qunit';
-import { setupRenderingTest } from 'app-ember/tests/helpers';
-import { render } from '@ember/test-helpers';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, fillIn, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | submit-form', function (hooks) {
+module('Integration | Component | transaction-form', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  test('it should submit the form with valid inputs', async function (assert) {
+    assert.expect(5);
 
-    await render(hbs`<SubmitForm />`);
-
-    assert.dom(this.element).hasText('');
-
-    // Template block usage:
     await render(hbs`
-      <SubmitForm>
-        template block text
-      </SubmitForm>
+      <SubmitForm />
     `);
 
-    assert.dom(this.element).hasText('template block text');
+    assert.dom('[data-test-account-input]').exists();
+    assert.dom('[data-test-amount-input]').exists();
+    assert.dom('[data-test-submit-form]').exists();
+    assert.dom('[data-test-generate-uuid]').exists();
+
+    await fillIn('#account-id', '02f16f8b-1878-4139-841a-89164d3756fc');
+    await fillIn('#amount', '100');
+    await click('[data-test-submit-form]');
+
+    assert.dom('[data-test-error]').doesNotExist();
+  });
+
+  test('it should not submit the form with invalid inputs', async function (assert) {
+    assert.expect(5);
+
+    await render(hbs`
+      <SubmitForm />
+    `);
+
+    assert.dom('[data-test-account-input]').exists();
+    assert.dom('[data-test-amount-input]').exists();
+    assert.dom('[data-test-submit-form]').exists();
+    assert.dom('[data-test-generate-uuid]').exists();
+
+    await fillIn('#account-id', 'abc');
+    await fillIn('#amount', '-10');
+    await click('[data-test-submit-form]');
+
+    assert.dom('[data-test-error]').hasText('Invalid account ID. Please enter a valid UUID.');
+  });
+
+  test('it should generate a UUID when button is clicked', async function (assert) {
+    assert.expect(6);
+
+    await render(hbs`
+      <SubmitForm/>
+    `);
+
+    assert.dom('[data-test-account-input]').exists();
+    assert.dom('[data-test-amount-input]').exists();
+    assert.dom('[data-test-submit-form]').exists();
+    assert.dom('[data-test-generate-uuid]').exists();
+    assert.dom('[data-test-account-input]').hasNoText();
+
+    await click('[data-test-generate-uuid]');
+
+    assert.dom('[data-test-account-input]').hasAnyValue();
   });
 });
